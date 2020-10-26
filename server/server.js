caf27e9a8ec0dd5ffd34b637308536f19a130b5c
Myron <dubtwee@gmail.com>
2020-10-26 09:38:17 -0500

    stubbing in router for todo interaction

--- server/server.js
+++ server/server.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+// imported routes
+const todosRouter = require('./routes/todos.router');
+
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+// middleware configuring
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// REGISTER ROUTES
+app.use('/todos', todosRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on PORT: ${PORT}`);
