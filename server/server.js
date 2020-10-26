5cb2ff7e6f6172638433066e084e4324d2a1cb5a
Myron <dubtwee@gmail.com>
2020-10-26 09:32:23 -0500

    stubbing in running server

--- server/server.js
+++ server/server.js
@@ -1,1 +1,9 @@
-console.log('LOG TEST');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on PORT: ${PORT}`);
+});
